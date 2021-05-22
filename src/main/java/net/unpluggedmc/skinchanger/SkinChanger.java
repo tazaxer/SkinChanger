af37dead094dacea3cc0d73dc8f95ea0cd7b5165
DennisUnplugged <dennis@tgfloat.com>
2021-05-22 13:12:58 +0300

    The dog woke up!

--- src/main/java/net/unpluggedmc/skinchanger/SkinChanger.java
+++ src/main/java/net/unpluggedmc/skinchanger/SkinChanger.java
@@ -15,7 +15,7 @@ public final class SkinChanger extends JavaPlugin {
         new CommandSkin(this);
         new CommandFoo(this);
         this.utils = new Utils(this);
-        getLogger().info("The dog fell asleep!");
+        getLogger().info("The dog woke up!");
 
     }
 
